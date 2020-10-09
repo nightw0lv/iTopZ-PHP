966040805f488d64760bde41f7ecc09996552870
nightw0lv <dotaddict69@hotmail.gr>
2020-10-10 01:01:22 +0300

    Update voteCheck.php
    Removed Json option, data comes always as json format.
    Updated server ID/Api Key to test server

--- voteCheck.php
+++ voteCheck.php
@@ -3,36 +3,23 @@
 // check if user voted
 // -------------------------------------------------
 $user_ip = '172.69.110.136'; // for debug you can use random (xxx.xxx.xxx.xxx) ip address
-$server_id = '325304'; // for debug you can use any id (int)
-$api_key = 'GBE29hUSpc-tOA7bPaYaaeddddErHaV'; // for debug you can use 'DEMO'
-$json = true;
-
-if ($json == true)
-	$url = "https://itopz.com/check/$api_key/$server_id/$user_ip/?json=true";
-else
-	$url = "https://itopz.com/check/$api_key/$server_id/$user_ip/";
+$server_id = '325339'; // for debug you can use any id (int)
+$api_key = 'd4IUPIZVgS-rkTvbL3fVGLEB5zhFdKq'; // for debug you can use 'DEMO'
+$url = "https://itopz.com/check/$api_key/$server_id/$user_ip/";
 
 echo 'has voted:' . getVote($url) . "<br>";
 // prints
-// Normal: has voted:FALSE
 // JSon: has voted:{ "voted": "FALSE" }
 
 // -------------------------------------------------
 // check total server votes
 // -------------------------------------------------
-$server_id = '325304'; // for debug you can use any id (int)
-$api_key = 'GBE29hUSpc-tOA7bPaYaaeddddErHaV'; // for debug you can use 'DEMO'
-$json = false;
-
-if ($json == true)
-	$url = "https://itopz.com/check/$api_key/$server_id/?json=true";
-else
-	$url = "https://itopz.com/check/$api_key/$server_id/";
+$server_id = '325339'; // for debug you can use any id (int)
+$api_key = 'd4IUPIZVgS-rkTvbL3fVGLEB5zhFdKq'; // for debug you can use 'DEMO'
+$url = "https://itopz.com/check/$api_key/$server_id/";
 
 echo 'Server votes:' . getVote($url) . "<br>";
-
 // prints
-// Normal: Server votes:Array ( [server_votes] => 248 [server_rank] => 1 [next_rank_votes] => 0 [next_rank] => 1 )
 // JSon: Server votes:{ "server_votes": 248, "server_rank": 1, "next_rank_votes": 0, "next_rank": 1 }
 
 /**
